9812616ccc306649beb83fb18c4350cd07ce7cc7
Thanh Nguyen <thanh94r10@gmail.com>
2018-03-11 16:37:40 +0700

    build

--- app/_services/index.ts
+++
@@ -1,7 +0,0 @@
-﻿export * from './pager.service';
-export * from './category.service';
-export * from './constant.service';
-export * from './lession.service';
-export * from './exam-questions.service';
-export * from './auth.service';
-export * from './localstorage.service';
